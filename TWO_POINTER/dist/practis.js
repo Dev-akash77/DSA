5d2a7b172fb04bb3cb3185d195705d847aaf4239
Akash Biswas <akashrahul2006@gmail.com>
2026-01-09 22:22:27 +0530

    sort colour problem solved with most optimal way

--- TWO_POINTER/dist/practis.js
+++ TWO_POINTER/dist/practis.js
@@ -52,5 +52,29 @@ const reverseString = (nums) => {
     return nums;
 };
 // console.log(reverseString(["h", "e", "l", "l", "o"])); // ["o","l","l","e","h"]
-// ! Three sum problem
+// ! SORT COLOURS
+function sortColour(nums) {
+    let low = 0;
+    let mid = 0;
+    let high = nums.length - 1;
+    while (mid <= high) {
+        if (nums[mid] === 0) {
+            [nums[mid], nums[low]] = [nums[low], nums[mid]];
+            mid++;
+            low++;
+        }
+        else if (nums[mid] === 1) {
+            mid++;
+        }
+        else {
+            [nums[mid], nums[high]] = [nums[high], nums[mid]];
+            high--;
+        }
+    }
+    return nums;
+}
+//? ----------------------------------TEST CASE---------------------------------------------------
+// sortColour([2,0,2,1,1,0]);
+console.log(sortColour([2, 0, 2, 1, 1, 0])); //[0,0,1,1,2,2]
+console.log(sortColour([2, 0, 1])); //[0,1,2]
 //# sourceMappingURL=practis.js.map
--- TWO_POINTER/practis.ts
+++ TWO_POINTER/practis.ts
@@ -62,6 +62,31 @@ const reverseString = (nums: string[]) => {
 
 // console.log(reverseString(["h", "e", "l", "l", "o"])); // ["o","l","l","e","h"]
 
+// ! SORT COLOURS
 
+function sortColour(nums: number[]): number[] {
+  let low = 0;
+  let mid = 0;
+  let high = nums.length - 1;
 
-// ! Three sum problem
+  while (mid <= high) {
+    if (nums[mid] === 0) {
+      [nums[mid]!, nums[low]!] = [nums[low]!, nums[mid]!];
+      mid++;
+      low++;
+    } else if (nums[mid] === 1) {
+      mid++;
+    } else {
+      [nums[mid!], nums[high]!] = [nums[high]!, nums[mid]!];
+      high--;
+    }
+  }
+
+  return nums;
+}
+//? ----------------------------------TEST CASE---------------------------------------------------
+
+// sortColour([2,0,2,1,1,0]);
+
+console.log(sortColour([2, 0, 2, 1, 1, 0])); //[0,0,1,1,2,2]
+console.log(sortColour([2, 0, 1])); //[0,1,2]
