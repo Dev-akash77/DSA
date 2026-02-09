a48d6af3c8cc38957bceafd6f996034c91d4c8dd
Akash Biswas <akashrahul2006@gmail.com>
2026-02-09 22:35:31 +0530

    Implement basic singly linked list with front push, end push, and print

--- Linkedlist/dist/learning.js
+++ Linkedlist/dist/learning.js
@@ -13,10 +13,39 @@ class LinkedList {
     constructor() {
         this.head = null;
     }
-    newHead(data) {
+    // ! push data at the end
+    endPush(data) {
+        const newNode = new Node(data);
+        if (this.head === null) {
+            return (this.head = newNode);
+        }
+        let current = this.head;
+        while (current.next) {
+            current = current.next;
+        }
+        current.next = newNode;
+    }
+    // ! push data at the front
+    frontPush(data) {
         const newNode = new Node(data);
         newNode.next = this.head;
         this.head = newNode;
     }
+    // ! print all linkedlist data
+    print() {
+        let current = this.head;
+        let result = "";
+        while (current != null) {
+            result += `[ ${current.data} | ${current.next ? "→" : "null"} ] `;
+            current = current.next;
+        }
+        console.log(result);
+    }
 }
+const list = new LinkedList();
+list.endPush(10);
+list.endPush(20);
+list.endPush(30);
+list.frontPush(5);
+list.print();
 //# sourceMappingURL=learning.js.map
--- Linkedlist/src/learning.ts
+++ Linkedlist/src/learning.ts
@@ -15,9 +15,48 @@ class LinkedList<T> {
     this.head = null;
   }
 
-  newHead(data: T) {
+  // ! push data at the end
+  endPush(data: T) {
     const newNode = new Node(data);
+
+    if (this.head === null) {
+      return (this.head = newNode);
+    }
+
+    let current = this.head;
+
+    while (current.next) {
+      current = current.next;
+    }
+    current.next = newNode;
+  }
+
+  // ! push data at the front
+  frontPush(data: T) {
+    const newNode = new Node(data);
+
     newNode.next = this.head;
     this.head = newNode;
   }
+
+  // ! print all linkedlist data
+  print(): void {
+    let current = this.head;
+    let result = "";
+
+    while (current != null) {
+      result += `[ ${current.data} | ${current.next ? "→" : "null"} ] `;
+      current = current.next;
+    }
+    console.log(result);
+  }
 }
+
+const list = new LinkedList<Number>();
+
+list.endPush(10);
+list.endPush(20);
+list.endPush(30);
+list.frontPush(5);
+
+list.print();
